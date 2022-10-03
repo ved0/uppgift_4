e55882dddd490e2d911503569983d83e3a3ca69f
Vedad <vedadz@gmail.com>
2022-10-03 20:45:49 +0200

    Added counter
    Added a counter of "done" items for the current list.
    
    Changed some texts.

--- main.js
+++ main.js
@@ -3,11 +3,14 @@ const taskInput = document.getElementById("task");
 const button = document.getElementById("addTask");
 const list = document.getElementById("taskList");
 const countingLabel = document.getElementById("counter");
+const currentCounting = document.getElementById("current-counter");
 const arrayLabel = document.getElementById("array");
 const fullArrayLabel = document.getElementById("fullArray");
-const arrayText = "The current array : ";
-const counterText = "Tasks done : ";
-const fullArrayText = "Array with all tasks : "
+const arrayText = "Array with current tasks : ";
+const currentCounter = "Done tasks on current list : ";
+const counterText = "All time tasks done : ";
+const fullArrayText = "Array with all time tasks : "
+let counter = 0;
 let arrayWithEverything = [];
 let counterOfEverything = 0;
 let doneTasksCounter = 0;
@@ -18,6 +21,7 @@ let arrayLastPoss = 0;
 countingLabel.textContent = counterText + doneTasksCounter;
 button.textContent = "Add task";
 inputLabel.textContent = "Input :";
+currentCounting.textContent = currentCounter + counter;
 arrayLabel.textContent = arrayText + "none";
 fullArrayLabel.textContent = fullArrayText + "none";
 
@@ -35,10 +39,14 @@ list.addEventListener("click", function (ev) {
     if (ev.target.tagName === "LI") {
         if (ev.target.classList.toggle("checked")) {
             ++doneTasksCounter;
+            ++counter;
             countingLabel.textContent = counterText + doneTasksCounter;
+            currentCounting.textContent = currentCounter + counter;
         } else {
             --doneTasksCounter;
+            --counter;
             countingLabel.textContent = counterText + doneTasksCounter;
+            currentCounting.textContent = currentCounter + counter;
         };
     }
 }, false);
@@ -46,8 +54,8 @@ list.addEventListener("click", function (ev) {
 function arrayToText(myArray) {
     let text = "";
     for (let i = 0; i < myArray.length; i++) {
-        if(i == myArray.length-1){
-            if(myArray.length > 4){
+        if (i == myArray.length - 1) {
+            if (myArray.length > 4) {
                 fullArrayLabel.style.height = "65px";
                 arrayLabel.style.height = "45px";
                 //work in progress.. would be great to auto-resize.
@@ -56,7 +64,7 @@ function arrayToText(myArray) {
         } else {
             text += myArray[i] + ", ";
         }
-    } if(text==""){
+    } if (text == "") {
         text = "none";
     }
     return text;
@@ -83,6 +91,10 @@ function whenClicked() {
             const index = taskArray.indexOf(newTask.textContent);
             taskArray.splice(index, 1);
             arrayLastPoss--;
+            if (counter > 0) {
+                --counter;
+                currentCounting.textContent = currentCounter + counter;
+            }
             arrayLabel.textContent = arrayText + arrayToText(taskArray);
             newTask.parentNode.removeChild(newTask);
         }
